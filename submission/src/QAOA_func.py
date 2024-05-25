47f580a3010e14b37be70d9cecc2072a52a52f3b
GuillaumeC <guillaumec@lptms-MacBook-Pro.local>
2024-05-25 21:43:55 +0200

    QAOA functions

--- submission/src/QAOA_func.py
+++ submission/src/QAOA_func.py
@@ -1,42 +1,38 @@
-
 import numpy as np
-import src.register_optimizer as regop
+import matplotlib.pyplot as plt
 from pulser import Pulse, Sequence, Register
-from pulser import Register
 from pulser_simulation import QutipEmulator
 from pulser.devices import DigitalAnalogDevice
 from pulser.waveforms import InterpolatedWaveform
 from scipy.optimize import minimize
 from scipy.spatial.distance import pdist, squareform
 
-def quantum_loop(parameters,parameter2,Q):
-    LAYERS=parameter2
-    
-    reg = regop.optimized_register(Q)
-    regop.draw_register(reg)
+def get_cost_colouring(bitstring, Q):
+    z = np.array(list(bitstring), dtype=int)
+    cost = z.T @ Q @ z
+    return cost
 
-    # Parametrized sequence
-    seq = Sequence(reg, DigitalAnalogDevice)
-    seq.declare_channel("ch0", "rydberg_global")
 
-    t_list = seq.declare_variable("t_list", size=LAYERS)
-    s_list = seq.declare_variable("s_list", size=LAYERS)
+def get_cost(counter, Q):
+    cost = sum(counter[key] * get_cost_colouring(key, Q) for key in counter)
+    return cost / sum(counter.values())  # Divide by total samples
 
-    for t, s in zip(t_list, s_list):
-        pulse_1 = Pulse.ConstantPulse(1000 * t, 1.0, 0.0, 0)
-        pulse_2 = Pulse.ConstantPulse(1000 * s, 0.0, 1.0, 0)
 
-    seq.add(pulse_1, "ch0")
-    seq.add(pulse_2, "ch0")
+def func(param, *args):
+    Q = args[0]
+    C = quantum_loop(param)
+    cost = get_cost(C, Q)
+    return cost
 
-    seq.measure("ground-rydberg")
-    params = np.array(parameters)
-    t_params, s_params = np.reshape(params.astype(int), (2, LAYERS))
-    assigned_seq = seq.build(t_list=t_params, s_list=s_params)
-    simul = QutipEmulator.from_sequence(assigned_seq, sampling_rate=0.01)
-    results = simul.run()
-    count_dict = results.sample_final_state()  # sample from the state vector
-    return count_dict
+def evaluate_mapping(new_coords, *args):
+    """Cost function to minimize. Ideally, the pairwise
+    distances are conserved"""
+    Q, shape = args
+    new_coords = np.reshape(new_coords, shape)
+    new_Q = squareform(
+        DigitalAnalogDevice.interaction_coeff / pdist(new_coords) ** 6
+    )
+    return np.linalg.norm(new_Q - Q)
 
 def plot_distribution(C):
     C = dict(sorted(C.items(), key=lambda item: item[1], reverse=True))
@@ -49,17 +45,4 @@ def plot_distribution(C):
     plt.xticks(rotation="vertical")
     plt.show()
 
-def get_cost_colouring(bitstring, Q):
-    z = np.array(list(bitstring), dtype=int)
-    cost = z.T @ Q @ z
-    return cost
 
-def get_cost(counter, Q):
-    cost = sum(counter[key] * get_cost_colouring(key, Q) for key in counter)
-    return cost / sum(counter.values())  # Divide by total samples
-
-def func(param, *args):
-    Q = args[0]
-    C = quantum_loop(param)
-    cost = get_cost(C, Q)
-    return cost
