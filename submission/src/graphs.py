963ef382896d11ecb29c39d0d9fc9a83a3a305b2
GuillaumeC <guillaumec@lptms-MacBook-Pro.local>
2024-05-26 11:12:06 +0200

    graphs

--- submission/src/graphs.py
+++ submission/src/graphs.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 def draw_random_graph(n, p):
     # Generate a random graph
@@ -8,13 +9,18 @@ def draw_random_graph(n, p):
 
 def square_graph(size):
     # Define the size of the square lattice 
-    rows, cols = size, size
-    G = nx.grid_2d_graph(rows, cols)
+    G = nx.grid_2d_graph(size,size)
     return (G)
 
-def plotting_square(G,size):
+def rectangle_graph(rows,cols):
+    # Define the size of the square lattice 
+    G = nx.grid_2d_graph(rows,cols)
+    return (G)
+
+def plotting_square(G):
     import src.graphs as graph
-    # G=graph.square_graph(size)
+
+    size=int(np.sqrt(G.number_of_nodes()))
 
     mapping = {}
     counter = 1
@@ -32,21 +38,13 @@ def plotting_square(G,size):
     nx.draw(G, pos, with_labels=True, labels=labels, node_size=500, node_color="lightblue", font_size=10, font_weight="bold", edge_color="gray")
 
 
-def plotting_random():
+def plotting_random(G):
     import src.graphs as graph
-    G=graph.square_graph(size)
-
-    mapping = {}
-    counter = 1
-    for i in range(size):
-        for j in range(size):
-            mapping[(i, j)] = counter
-            counter += 1
-
-    #Create labels using the mapping
-    labels = {node: str(mapping[node]) for node in G.nodes()}
+    
+    n=G.number_of_nodes()
+    pos = nx.spring_layout(G)
 
     #Draw the 2D grid graph
     pos = dict((n, n) for n in G.nodes())  # Use node coordinates as positions
 
-    nx.draw(G, pos, with_labels=True, labels=labels, node_size=500, node_color="lightblue", font_size=10, font_weight="bold", edge_color="gray")
+    nx.draw(G, pos, with_labels=True, node_color="lightblue", node_size=500, font_size=10, font_weight="bold", edge_color="gray")
